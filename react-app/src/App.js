53138a9b7ae31d32206dc3c811d18243ea7964bb
Rebecca <rebeccawchen@gmail.com>
2021-06-22 13:15:47 -0500

    base landing page styles

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,6 +10,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import SearchResults from "./components/search/SearchResults";
+import LandingPage from "./components/LandingPage/LandingPage"
 import { authenticate } from "./store/session";
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
       <Switch>
         {/* Removed protected route so it wouldn't redirect to login */}
         <Route exact path="/" exact={true} >
-          <h1>Splash Page</h1>
+          <LandingPage />
         </Route>
         <Route path="/login" exact={true}>
           <LoginForm />
