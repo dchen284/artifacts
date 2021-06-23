e0482afcede94f3c9b96274d85b635babaea68cc
Danny <dchen284@gmail.com>
2021-06-23 16:24:40 -0500

    continue work

--- react-app/src/components/ShoppingCart/ShoppingCart.js
+++ react-app/src/components/ShoppingCart/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 import ShoppingCartItem from './ShoppingCartItem';
 import CheckoutModal from './CheckoutModal';
@@ -11,9 +11,41 @@ const ShoppingCart = () => {
     // const dispatch = useDispatch();
     // const cartItems = useSelector(state => state.cart);
 
-    // cartItems = [
+    const cartItems = [
+        { quantity: 2, product: {quantity: 3} },
+        { quantity: 2, product: {quantity: 3} },
+    ];
+    // cartItems will be an array of objects, objects with the form:
+    /*
+        {
+            "id": self.id,
+            "userId": self.userId,
+            "productId": self.productId,
+            "quantity": self.quantity,
+            "product": self.products.to_dict()
+        }
+    */
 
-    // ]
+    //useEffects
+
+    /*
+    This useEffect toggles the disabled status of the Checkout button.
+
+    In the useEffect, the Checkout button starts as disabled=false,
+    then changes to disabled=true if any of the following problems occur:
+
+    1) A cart item's quantity is larger than the product's quantity (the available inventory).
+    2) A cart item's quantity is less than 1.  (0 should be deleted, negative numbers not possible)
+    */
+
+    useEffect(() => {
+        setCheckoutIsDisabled(false);
+        cartItems.forEach( cartItem => {
+            if (cartItem.quantity > cartItem.product.quantity || cartItem.quantity < 1) {
+                setCheckoutIsDisabled(true);
+            }
+        });
+    }, [cartItems]);
 
     //functions
     // const checkout = () => {
@@ -25,7 +57,7 @@ const ShoppingCart = () => {
         <div className="shopping_cart">
             <div className="shopping_cart_item_display">
                 <div className="shopping_cart_item_display__title">Shopping Cart</div>
-                <ShoppingCartItem setCheckoutIsDisabled={setCheckoutIsDisabled}/>
+                <ShoppingCartItem />
                 <ShoppingCartItem />
                 <ShoppingCartItem />
                 <ShoppingCartItem />
--- react-app/src/components/ShoppingCart/ShoppingCartItem.js
+++ react-app/src/components/ShoppingCart/ShoppingCartItem.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './ShoppingCart.css';
 
-const ShoppingCartItem = ({setCheckoutIsDisabled}) => {
+const ShoppingCartItem = () => {
+
+    const cartItem = { quantity: 2, product: {quantity: 4}};
 
     //hooks and state
-    const [quantity, setQuantity] = useState(1);
-
+    const [quantity, setQuantity] = useState(cartItem.quantity);
+    const [quantityStatus, setQuantityStatus] = useState();
+    const [quantityError, setQuantityError] = useState(false);
+    // const dispatch = useDispatch();
+    // let quantityStatus;
     //to do
 
     //functions
@@ -22,10 +28,48 @@ const ShoppingCartItem = ({setCheckoutIsDisabled}) => {
     // so on the Shopping Cart, it will get all the cart items from store
 
     // useEffects
+
+    /*
+    This function updates the Redux store that the quantity of an item has changed.
+    */
+    const changeQuantity = (e) => {
+        if (e.target.value > cartItem.product.quantity) {
+            setQuantity(cartItem.product.quantity);
+            setQuantityError('Quantity exceeds stock quantity, please adjust.');
+            setTimeout(() => {setQuantityError('')}, 5000);
+            //dispatch(fetchChangeQuantityOfItem(quantity));
+        }
+        else if (e.target.value < 1) {
+            setQuantity(1);
+            setQuantityError('Quantity must be 1 or greater; use Delete Item button to delete.');
+            setTimeout(() => {setQuantityError('')}, 5000);
+            //dispatch(fetchChangeQuantityOfItem(1));
+        }
+        else {
+            setQuantity(e.target.value);
+            //dispatch(fetchChangeQuantityOfItem(quantity));
+        }
+
+    }
+
     useEffect(() => {
-        console.log(setCheckoutIsDisabled);
-        quantity > 3 ? console.log(true) : console.log(false);
-    }, [quantity])
+        if (cartItem.product.quantity === 0) {
+            setQuantityStatus(`In Stock: ${cartItem.product.quantity} [Sold Out]`);
+        }
+        else if (cartItem.product.quantity <= 5) {
+            setQuantityStatus(`In Stock: ${cartItem.product.quantity} [Almost out, order now!]`);
+        }
+        else {
+            setQuantityStatus(`In Stock: ${cartItem.product.quantity}`);
+        }
+    }, [cartItem]);
+
+    /*
+    This function updates the Redux store that an item has been removed.
+    */
+    const deleteCartItem = (e) => {
+
+    }
 
 
     //JSX
@@ -41,28 +85,35 @@ const ShoppingCartItem = ({setCheckoutIsDisabled}) => {
                     </Link>
                 </div>
                 <div className="shopping_cart_item__line">
-                    <span className="shopping_cart_item__stock_yes">Stock:
+                    <div>{quantityStatus}</div>
+                    {/* <span className="shopping_cart_item__stock_yes">Stock:
                         <span>3</span>
                     /</span>
                     <span className="shopping_cart_item__stock_low">Almost Out, Order Now! /</span>
-                    <span className="shopping_cart_item__stock_no">Out of Stock</span>
+                    <span className="shopping_cart_item__stock_no">Out of Stock</span> */}
                 </div>
                 <div className="shopping_cart_item__line">
                     <label htmlFor="quantity" className="shopping_cart_item__quantity_label">Quantity:</label>
                     <input
                         type="number"
                         value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
+                        onChange={changeQuantity}
                         name="quantity"
                         className="shopping_cart_item__quantity_number"
                     />
+
                 </div>
+                <div className="shopping_cart_item__quantity_error">{quantityError}</div>
                 <div className="shopping_cart_item__line shopping_cart_item__priceline">
                     <div>Total</div>
                     <div>$0.00</div>
                 </div>
                 <div className="shopping_cart_item__line">
-                    <button>Delete Item</button>
+                    <button
+                        onClick={deleteCartItem}
+                    >
+                        Delete Item
+                    </button>
                 </div>
             </div>
 
