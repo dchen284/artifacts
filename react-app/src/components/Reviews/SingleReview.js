07a75f3a4bbfd3f6001c5a8c5920058679c4aaed
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-06-22 19:47:22 -0700

    form skeleton

--- react-app/src/components/Reviews/SingleReview.js
+++ react-app/src/components/Reviews/SingleReview.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { getReviews, deleteReview } from '../../store/reviews';
-import './Review.css';
+import './ReviewForm.css';
 
 
 function SingleReview() {
@@ -11,6 +11,12 @@ function SingleReview() {
     const dispatch = useDispatch();
     const reviewState = useSelector(state => state.reviews)
     const reviews = Object.values(reviewState);
+    
+
+    const [showForm, setShowForm] = useState(false);
+    const [rating, setRating] = useState(0);
+    const [content, setContent] = useState('');
+
 
     useEffect(() => {
         dispatch(getReviews())
@@ -22,13 +28,12 @@ function SingleReview() {
     }
 
     const openForm = () => {
-        document.getElementById("myForm").style.display = 'block';
+        setShowForm(!showForm)
     }
 
-    const closeForm = () => {
-        document.getElementById("myForm").style.display = 'none';
+    const handleSubmit = async (e) => {
+        e.preventDefault()
     }
-
 
     return (
         <div className='outer-div'>
@@ -44,14 +49,36 @@ function SingleReview() {
                         <button onClick={() => history.push(`/reviews/review_form/${review.productId}`)}>Review Form</button>
                     </div>
                     <div>
-                        <button className='open-edit-form' onClick={openForm()}>Edit Review</button>
+                        <button className='open-edit-form' onClick={openForm}>Edit Review</button>
                     </div>
                     <div>
                         <button onClick={() => handleDelete(review.id)}>Delete Review</button>
                     </div>
+                    {showForm && (
                     <div className='form-popup' id='myForm'>
-
+                        <form className='review-edit-form' onSubmit={handleSubmit}>
+                            <h2>Edit Your Review</h2>
+                            <div className="rate">
+                                <input type="radio" id="star5" name="rate" value="5" onChange={(e) => setRating(e.target.value)}/>
+                                <label htmlFor="star5" title="text">5 stars</label>
+                                <input type="radio" id="star4" name="rate" value="4" onChange={(e) => setRating(e.target.value)} />
+                                <label htmlFor="star4" title="text">4 stars</label>
+                                <input type="radio" id="star3" name="rate" value="3" onChange={(e) => setRating(e.target.value)} />
+                                <label htmlFor="star3" title="text">3 stars</label>
+                                <input type="radio" id="star2" name="rate" value="2" onChange={(e) => setRating(e.target.value)} />
+                                <label htmlFor="star2" title="text">2 stars</label>
+                                <input type="radio" id="star1" name="rate" value="1" onChange={(e) => setRating(e.target.value)} />
+                                <label htmlFor="star1" title="text">1 star</label>
+                            </div>
+                            <br></br>
+                            <br></br>
+                            <div className='review-box'>
+                                <textarea placeholder='Your Review' rows='10' cols='50' value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                            </div>
+                            <button type='submit' className='submit-button'>Submit</button>
+                        </form>
                     </div>
+                    )}
                 </div>)}
             </div>
         </div>
