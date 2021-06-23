4ddc37ea06a99c10ed133b84f5e5f8b25f7d68e2
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-06-23 15:08:39 -0700

    editing divs

--- react-app/src/components/Reviews/SingleReview.js
+++ react-app/src/components/Reviews/SingleReview.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { getReviews } from '../../store/reviews';
+import ReviewShowForm from './ReviewShowForm'
+import ReviewForm from './ReviewForm';
 import './ReviewForm.css';
 
-import ReviewShowForm from './ReviewShowForm'
 
 
 function SingleReview() {
@@ -15,6 +16,15 @@ function SingleReview() {
     const reviews = Object.values(reviewState);
     const { productId } = useParams()
 
+    const [showForm, setShowForm] = useState(false);
+
+    const openForm = () => {
+    reviews.map((review) => {
+        if(user.id == review.userId) {
+            setShowForm(!showForm)
+        }
+    })
+    }
 
     useEffect(() => {
         dispatch(getReviews())
@@ -23,7 +33,12 @@ function SingleReview() {
     return (
         <div className='outer-div'>
             <div>
-                <button className='add-review-button' onClick={() => history.push(`/reviews/review_form/${productId}`)}>Add A Review</button>
+                <div>
+                    <button className='new-review' onClick={openForm}>Write A Review</button>
+                    {showForm &&
+                        <ReviewForm />
+                    }
+                </div>
                 {reviews.map((review) =>
                     <ReviewShowForm review={review}/>
                 )}
