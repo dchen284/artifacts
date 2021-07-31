b3d9dfad06ee57c88a460683420bac2811540143
Danny <dchen284@gmail.com>
2021-07-31 16:26:46 -0500

    on review forms, show initial start status; add required attribute to textareas

--- react-app/src/components/Reviews/ReviewForm.js
+++ react-app/src/components/Reviews/ReviewForm.js
@@ -43,15 +43,15 @@ return (
             <br></br>
             <h2 className='review-header'>Leave Us Your Feedback</h2>
             <div className="rate">
-                <input type="radio" id="star5" name="rate" value="5" onChange={(e) => setRating(e.target.value)}/>
+                <input type="radio" id="star5" name="rate" value="5" onChange={(e) => setRating(+e.target.value)} checked={rating===5} />
                 <label htmlFor="star5" title="text">5 stars</label>
-                <input type="radio" id="star4" name="rate" value="4" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star4" name="rate" value="4" onChange={(e) => setRating(+e.target.value)} checked={rating===4} />
                 <label htmlFor="star4" title="text">4 stars</label>
-                <input type="radio" id="star3" name="rate" value="3" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star3" name="rate" value="3" onChange={(e) => setRating(+e.target.value)} checked={rating===3} />
                 <label htmlFor="star3" title="text">3 stars</label>
-                <input type="radio" id="star2" name="rate" value="2" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star2" name="rate" value="2" onChange={(e) => setRating(+e.target.value)} checked={rating===2} />
                 <label htmlFor="star2" title="text">2 stars</label>
-                <input type="radio" id="star1" name="rate" value="1" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star1" name="rate" value="1" onChange={(e) => setRating(+e.target.value)} checked={rating===1} />
                 <label htmlFor="star1" title="text">1 star</label>
             </div>
             <br></br>
--- react-app/src/components/Reviews/UpdateReviewForm.js
+++ react-app/src/components/Reviews/UpdateReviewForm.js
@@ -13,8 +13,8 @@ function UpdateReviewForm({ reviewId, openForm, review }) {
     const reviews = Object.values(reviewState);
     const { productId } = useParams()
 
-    const [rating, setRating] = useState(0);
-    const [content, setContent] = useState('');
+    const [rating, setRating] = useState(review.rating);
+    const [content, setContent] = useState(review.content);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -43,20 +43,20 @@ function UpdateReviewForm({ reviewId, openForm, review }) {
             <br></br>
             <h2 className='edit-form-title'>Edit Your Review</h2>
             <div className="rate">
-                <input type="radio" id="star5" name="rate" value="5" onChange={(e) => setRating(e.target.value)}/>
+                <input type="radio" id="star5" name="rate" value="5" onChange={(e) => setRating(+e.target.value)} checked={rating===5} />
                 <label htmlFor="star5" title="text">5 stars</label>
-                <input type="radio" id="star4" name="rate" value="4" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star4" name="rate" value="4" onChange={(e) => setRating(+e.target.value)} checked={rating===4} />
                 <label htmlFor="star4" title="text">4 stars</label>
-                <input type="radio" id="star3" name="rate" value="3" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star3" name="rate" value="3" onChange={(e) => setRating(+e.target.value)} checked={rating===3} />
                 <label htmlFor="star3" title="text">3 stars</label>
-                <input type="radio" id="star2" name="rate" value="2" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star2" name="rate" value="2" onChange={(e) => setRating(+e.target.value)} checked={rating===2} />
                 <label htmlFor="star2" title="text">2 stars</label>
-                <input type="radio" id="star1" name="rate" value="1" onChange={(e) => setRating(e.target.value)} />
+                <input type="radio" id="star1" name="rate" value="1" onChange={(e) => setRating(+e.target.value)} checked={rating===1} />
                 <label htmlFor="star1" title="text">1 star</label>
             </div>
             <br></br>
             <div className='review-box-edit'>
-                <textarea placeholder={review.content} rows='10' cols='50' value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                <textarea placeholder={review.content} rows='10' cols='50' value={content} onChange={(e) => setContent(e.target.value)} required></textarea>
             </div>
             <button type='submit' className='submit-button-edit'>Submit</button>
             <button className='cancel-button-edit' onClick={openForm}>Cancel</button>
