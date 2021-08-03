e588e72edd93216ea06d3dd43e4c6e065cbee97b
Danny <dchen284@gmail.com>
2021-08-03 09:39:23 -0500

    add 404 message for product not found

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -70,7 +70,7 @@ function App() {
           <SearchResults />
         </Route>
         <Route>
-          <h1>This does not exist across all of time (404).</h1>
+          <h1>This page does not exist across all of time (404).</h1>
         </Route>
       </Switch>
     </BrowserRouter>
--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { retrieveProduct } from '../../store/products';
@@ -10,13 +10,24 @@ const ProductPage = () => {
   const dispatch = useDispatch();
   // const product = useSelector(state => state.products)
   const product = useSelector(state => state.products[productId])
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     dispatch(retrieveProduct(productId))
+      .then(() => {
+        setIsLoaded(true);
+      })
+      .catch((e)=>{
+        setIsLoaded(true);
+      })
   }, [dispatch, productId]);
 
-  if (!product) {
+  if (!isLoaded) {
     return null;
+  }
+
+  if (isLoaded && !product) {
+    return <h1>This product does not exist across all of time (404).</h1>
   }
 
   return (
